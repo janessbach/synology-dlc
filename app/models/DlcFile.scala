152c7bdbf140603f13f289b46cf81c3e707d1baa
Elisa Baum <elisa.baum@adesso.de>
2016-04-06 16:26:35 +0200

    add DlcFile and service

---
+++ app/models/DlcFile.scala
@@ -0,0 +1,3 @@
+package models
+
+case class DlcFile(fileName: String, url: String)
---
+++ app/services/DlcExtractorService.scala
@@ -0,0 +1,10 @@
+package services
+
+import models.DlcFile
+import java.io.File
+
+class DlcExtractorService {
+
+  def extract(file: File): List[DlcFile] = ???
+
+}
