3351330442b9c8a3b2b6875849ea361d1d5c4edb
Elisa Baum <elisa.baum@adesso.de>
2016-04-06 16:59:11 +0200

    service implementation

--- app/services/DlcExtractorService.scala
+++ app/services/DlcExtractorService.scala
@@ -3,8 +3,26 @@ package services
 import models.DlcFile
 import java.io.File
 
+import scala.collection.JavaConversions._
+import de.itgecko.dlc.decrypter.DLCDecrypter
+import play.api.Logger
+
+import scala.util.Try
+
 class DlcExtractorService {
 
-  def extract(file: File): List[DlcFile] = ???
+  val logger = Logger(classOf[DlcExtractorService])
+
+  def extract(file: File): List[DlcFile] = {
+
+    Try(DLCDecrypter.decrypt(file).getDlcFiles).map { files =>
+      files.map(file => new DlcFile(file.getFilename, file.getUrl))
+    }.recover {
+      case e: Exception =>
+        logger.error(s"could not extract dlc file ${file.getAbsolutePath}", e)
+        List.empty
+    }.get.toList
+
+  }
 
 }
