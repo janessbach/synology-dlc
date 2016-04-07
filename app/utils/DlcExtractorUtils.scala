95021ed490b0ddfb106a2811c395fef561d4576c
Elisa Baum <elisa.baum@adesso.de>
2016-04-07 14:48:45 +0200

    clean up

--- app/utils/DlcExtractorUtils.scala
+++ app/utils/DlcExtractorUtils.scala
@@ -1,7 +1,6 @@
 package utils
 
 import models.DlcFile
-import models.DlcFile._
 import de.itgecko.dlc.decrypter.DLCDecrypter
 
 import play.api.Logger
@@ -12,15 +11,11 @@ import java.io.File
 
 object DlcExtractorUtils {
 
-  val logger = Logger(DlcExtractorUtils.getClass)
+  private val logger = Logger(DlcExtractorUtils.getClass)
 
-  def extract(file: File): List[DlcFile] = {
-
-    Try(DLCDecrypter.decrypt(file).getDlcFiles.toList).map(apply(_)) match {
-      case Success(v) => v
-      case Failure(e) => logger.error("could not extract dlc file", e); List.empty
-    }
-
+  def extract(file: File): List[DlcFile] = Try(DLCDecrypter.decrypt(file).getDlcFiles.toList) match {
+    case Success(files) => DlcFile.apply(files)
+    case Failure(error) => logger.error("could not extract dlc file", error); List.empty
   }
 
 }
