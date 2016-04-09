a7fcaa7fa2379c346ce759e13817398dfa8ff1a0
Elisa Baum <elisa.baum@adesso.de>
2016-04-09 17:29:19 +0200

    first try of remote file service factory

--- app/modules/dlc/services/RemoteFileService.scala
+++ app/modules/dlc/services/RemoteFileService.scala
@@ -3,13 +3,29 @@ package modules.dlc.services
 import javax.inject.Inject
 
 import modules.dlc.models.RemoteFile
+import play.api.Logger
 import play.api.libs.ws.WSClient
 
 import scala.concurrent.{ExecutionContext, Future}
 
-abstract class RemoteFileService @Inject() (wsClient : WSClient)(implicit ec: ExecutionContext) {
-
+trait RemoteFileService {
   def checkAvailability(file: RemoteFile): Future[Boolean]
+}
+
+class RemoteFileServiceFactory @Inject() (wsClient : WSClient)(implicit ec: ExecutionContext) {
+
+  private val logger = Logger(classOf[RemoteFileServiceFactory])
+
+  def checkAvailability(file: RemoteFile): Future[Boolean] = {
+    val fileService =  mapping.find { case(key,value) => file.url.startsWith(key) }.map(_._2)
+
+    fileService.map { service =>
+      service.checkAvailability(file)
+    } getOrElse {
+      logger.error(s"unsupported file hoster for url: ${file.url}")
+      Future.failed(new UnsupportedFileHosterException)
+    }
+  }
 
   def mapping = {
     Map(
@@ -17,5 +33,6 @@ abstract class RemoteFileService @Inject() (wsClient : WSClient)(implicit ec: Ex
       "http://ul.to" -> new UploadedToFileService(wsClient)
     )
   }
-
 }
+
+class UnsupportedFileHosterException extends Exception
