92d2822e5ff18ebdfb1d29878bfcde01300ff7d5
Jan Essbach <jan.essbach@imoveit.de>
2016-04-10 21:17:18 +0200

    fix

--- app/modules/synology/client/ClientApi.scala
+++ app/modules/synology/client/ClientApi.scala
@@ -14,7 +14,7 @@ class ClientApi @Inject() (wsClient : WSClient,
   private val ApiPrefix = "http://192.168.1.2:5000"
 
   private def loginCall[A](username: String, password: String)(implicit reads: Reads[A]) = retrieve(
-    uri = s"/webapi/auth.cgi?api=SYNO.API.Auth&version=2&method=login&account=${UriEncoding.decodePathSegment(username, "utf-8")}&passwd=${UriEncoding.decodePathSegment(password, "utf-8")}&session=DownloadStation&format=cookie"
+    uri = s"/webapi/auth.cgi?api=SYNO.API.Auth&version=2&method=login&account=${UriEncoding.encodePathSegment(username, "utf-8")}&passwd=${UriEncoding.encodePathSegment(password, "utf-8")}&session=DownloadStation&format=cookie"
   )
 
   private def logoutCall[A](implicit reads: Reads[A]) = retrieve(
