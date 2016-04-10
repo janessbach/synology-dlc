e04ea6244a60946d2f4dad37ed036154a2ec2d51
Jan Essbach <jan.essbach@imoveit.de>
2016-04-10 21:16:39 +0200

    more refinements

--- app/modules/synology/client/ClientApi.scala
+++ app/modules/synology/client/ClientApi.scala
@@ -5,19 +5,20 @@ import modules.auth.models.{LoginStatus, LogoutStatus}
 import play.api.Configuration
 import play.api.libs.json._
 import play.api.libs.ws.WSClient
-
+import play.utils.UriEncoding
 import scala.concurrent.{ExecutionContext, Future}
 
 class ClientApi @Inject() (wsClient : WSClient,
-                           config: ClientApiConfiguration)(implicit ec: ExecutionContext)  {
+                           config: ClientApiConfiguration)(implicit ec: ExecutionContext) {
 
   private val ApiPrefix = "http://192.168.1.2:5000"
 
-  private def loginCall[A](username: String, password: String)(implicit reads: Reads[A]) = retrieve(ApiPrefix +
-    s"/webapi/auth.cgi?api=SYNO.API.Auth&version=2&method=login&account=$username&passwd=$password&session=DownloadStation&format=cookie")
+  private def loginCall[A](username: String, password: String)(implicit reads: Reads[A]) = retrieve(
+    uri = s"/webapi/auth.cgi?api=SYNO.API.Auth&version=2&method=login&account=${UriEncoding.decodePathSegment(username, "utf-8")}&passwd=${UriEncoding.decodePathSegment(password, "utf-8")}&session=DownloadStation&format=cookie"
+  )
 
-  private def logoutCall[A](implicit reads: Reads[A]) = retrieve(ApiPrefix +
-    s"/webapi/auth.cgi?api=SYNO.API.Auth&version=1&method=logout&session=DownloadStation"
+  private def logoutCall[A](implicit reads: Reads[A]) = retrieve(
+    uri = s"/webapi/auth.cgi?api=SYNO.API.Auth&version=1&method=logout&session=DownloadStation"
   )
 
   def login(username: String, password: String) : Future[LoginStatus] = {
@@ -32,14 +33,14 @@ class ClientApi @Inject() (wsClient : WSClient,
     import LogoutStatus._
 
     logoutCall map { _ getOrElse LogoutDone }
-
   }
 
-  private def retrieve[A](uri: String)(implicit reads: Reads[A]) : Future[Option[A]] = wsClient.url(uri)
-    .get().map { response =>
+  private def retrieve[A](uri: String)(implicit reads: Reads[A]) : Future[Option[A]] = wsClient
+    .url(ApiPrefix + uri)
+    .get()
+    .map { response =>
       Json.parse(response.body).validate[A] match {
-        case JsSuccess(v, path) => Some(v)
-        case e: JsError => None
+        case JsSuccess(v, path) => Some(v); case e: JsError => None
       }
     }
 }
