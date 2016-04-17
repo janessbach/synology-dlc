88191ea7e12ff54c19634c40da2d9adb95a613e1
Elisa Baum <elisa.baum@adesso.de>
2016-04-17 11:22:29 +0200

    improvements from pr

--- app/modules/synology/services/SynologyService.scala
+++ app/modules/synology/services/SynologyService.scala
@@ -2,20 +2,19 @@ package modules.synology.services
 
 import com.google.inject.Inject
 import modules.core.auth.models.{LoginStatus, LogoutStatus, User}
-import modules.synology.client.{ClientApi, DownloadApi}
+import modules.synology.client.SynologyClient
 import modules.synology.models.downloads.{DownloadStatus, Downloads}
 
 import scala.concurrent.Future
 
-class SynologyService @Inject() (client : ClientApi,
-                                 downloadApi: DownloadApi) {
+class SynologyService @Inject() (client : SynologyClient) {
 
   def login(username: String, password: String) : Future[LoginStatus] = client.login(username, password)
 
   def logout(user: User) : Future[LogoutStatus] = client.logout(user.loginStatus)
 
-  def downloads(user: User): Future[Downloads] = downloadApi.listDownloads(user.loginStatus)
+  def downloads(user: User): Future[Downloads] = client.listDownloads(user.loginStatus)
 
-  def download(user: User)(urls: List[String]): Future[DownloadStatus] =  downloadApi.download(user.loginStatus)(urls)
+  def download(user: User)(urls: List[String]): Future[DownloadStatus] =  client.download(user.loginStatus)(urls)
 
 }
