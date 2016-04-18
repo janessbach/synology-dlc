ccbfaeb6a9cbd3a12ab7a4b962c33da3efc8e963
Jan Essbach <jan.essbach@imoveit.de>
2016-04-18 20:26:43 +0200

    some more refactoring

--- app/controllers/ApiController.scala
+++ app/controllers/ApiController.scala
@@ -8,7 +8,7 @@ import modules.core.controllers.CoreController
 import modules.core.notification.services.NotificationService
 import modules.dlc.models.RemoteFile
 import modules.dlc.services.{DlcExtractorService, RemoteFileServiceFactory}
-import platform.services.DownloadService
+import modules.synology.services.SynologyService
 import play.api.libs.json.{Json, _}
 import play.api.mvc.{Action, AnyContent, _}
 
@@ -17,10 +17,17 @@ import scala.concurrent.{ExecutionContext, Future}
 class ApiController @Inject()(notificationService: NotificationService,
                               remoteFileService: RemoteFileServiceFactory,
                               dlcExtractorService: DlcExtractorService,
-                              downloadService: DownloadService)
+                              synologyService: SynologyService)
                              (implicit exec: ExecutionContext, actorSystem: ActorSystem, materializer: Materializer) extends CoreController {
 
   private val HtmlDlcInputName = "dlc-file"
+
+  def addDownloads = BaseAction(parse.json[List[RemoteFile]]) { implicit context =>
+    synologyService.addDownloads(context.body) map {
+      case status if status => Ok(Json.toJson(status))
+      case _ => BadRequest
+    }
+  }
 
   def available(url: String) = BaseAction { implicit context =>
     remoteFileService.checkAvailability(RemoteFile.url(url)) map { available =>
@@ -31,6 +38,13 @@ class ApiController @Inject()(notificationService: NotificationService,
     }
   }
 
+  def currentDownloads: Action[AnyContent] = BaseAction { implicit context =>
+    synologyService.currentDownloads(context.user) map {
+      case Some(downloads) => Ok(Json.toJson(downloads))
+      case _ => BadRequest
+    }
+  }
+
   def dlcDecrypt = BaseAction(parse.multipartFormData) { implicit context =>
     context.request.body.file(HtmlDlcInputName) map { item =>
       val file = item.ref.file
@@ -38,21 +52,8 @@ class ApiController @Inject()(notificationService: NotificationService,
     } getOrElse Future.successful(BadRequest)
   }
 
-  def downloads: Action[AnyContent] = BaseAction { implicit context =>
-    downloadService.downloads(context.user) map {
-      case Some(downloads) => Ok(Json.toJson(downloads))
-      case _ => BadRequest
-    }
-  }
-
-  def download(urls: List[String]) = BaseAction { implicit context =>
-    downloadService.download(context.user)(urls) map {
-      case status if status => Ok(Json.toJson(status))
-      case _ => BadRequest
-    }
-  }
-
   def subscribe = WebSocket.accept[JsValue, JsValue] { request => notificationService.flow }
 
 }
+
 
--- app/modules/synology/services/SynologyService.scala
+++ app/modules/synology/services/SynologyService.scala
@@ -2,19 +2,34 @@ package modules.synology.services
 
 import com.google.inject.Inject
 import modules.core.auth.models.{LoginStatus, LogoutStatus, User}
+import modules.core.controllers.RequestContext
+import modules.dlc.models.RemoteFile
 import modules.synology.client.SynologyClient
 import modules.synology.models.downloads.{DownloadStatus, Downloads}
+import platform.models.Download
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
-class SynologyService @Inject() (client : SynologyClient) {
+class SynologyService @Inject() (client : SynologyClient)(implicit ec: ExecutionContext) {
 
-  def addDownloads(user: User)(urls: List[String]): Future[DownloadStatus] =  client.addDownloads(user.loginStatus)(urls)
+  def addDownloads[A](files: List[RemoteFile])(implicit context : RequestContext[A]): Future[Boolean] = client
+    .addDownloads(context.user.loginStatus)(files.map(_.url))
+    .map(_.success)
 
-  def currentDownloads(user: User): Future[Downloads] = client.currentDownloads(user.loginStatus)
+  // RETHINK ABOUT THIS DATA TYPE
+  def currentDownloads(user: User): Future[Option[List[Download]]] = client
+    .currentDownloads(user.loginStatus)
+    .map { downloads =>
+      if (downloads.success)
+        Option(downloads.files.map(file => Download(id = file.id, size = file.size, status = file.status, title = file.title)))
+      else
+        None
+    }
 
-  def login(username: String, password: String) : Future[LoginStatus] = client.login(username, password)
+  def login(username: String, password: String) : Future[LoginStatus] =
+    client.login(username, password)
 
-  def logout(user: User) : Future[LogoutStatus] = client.logout(user.loginStatus)
+  def logout(user: User) : Future[LogoutStatus] =
+    client.logout(user.loginStatus)
 
 }
