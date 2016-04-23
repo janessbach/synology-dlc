61104a2ec42d39b421206a6f27fdd37d2b23ae76
Jan-Gerrit Essbach <jan.essbach@adesso.de>
2016-04-23 13:20:41 +0200

    simple event stream job

---
+++ app/platform/jobs/SynologyHostFinder.scala
@@ -0,0 +1,33 @@
+package platform.jobs
+
+import akka.actor.ActorSystem
+import akka.stream.Materializer
+import com.google.inject.{Inject, Singleton}
+import modules.core.notification.models.Notification
+import modules.core.notification.services.NotificationService
+import play.api.libs.json.JsString
+import play.api.libs.ws.WSClient
+
+import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.duration._
+
+@Singleton
+class SynologyHostFinder @Inject()(actorSystem : ActorSystem,
+                                   notificationService: NotificationService,
+                                   wsClient: WSClient)(implicit ec: ExecutionContext, materializer: Materializer) {
+
+  var currentIpAddress : Option[String] = None
+  val cancellable = actorSystem.scheduler.schedule(0.seconds, 1.seconds)(runJob)
+
+  /**
+    * This Job is run every 3 Seconds and it's result
+    * is pushed via web sockets to all subscribers.
+    */
+  private def runJob : Unit = {
+    val notification = Notification("synology_host_finder", JsString("Hello World"))
+    notificationService.pushNotification(notification)
+  }
+
+  private def checkService(host: String): Future[Boolean] = wsClient.url(s"http://${host}").get().map(_.status == 200)
+
+}
